ed5375c32f007944ae344d5e886c2a4f9f7f50d5
André Mion <andremion@gmail.com>
2016-06-17 18:27:02 +0100

    Update copyright header

--- app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
+++ app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016. André Mion
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sample.andremion.musicplayer.activities;
 
 import android.content.Intent;
@@ -40,7 +56,7 @@ public class MainActivity extends PlayerActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.tracks);
         assert recyclerView != null;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new RecyclerViewAdapter(MusicContent.ITEMS, null));
+        recyclerView.setAdapter(new RecyclerViewAdapter(MusicContent.ITEMS));
     }
 
     public void onFabClick(View view) {
