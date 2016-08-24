e703d808075bb440717b0fee7f79061ee9c05361
André Mion <andremion@gmail.com>
2016-08-24 12:27:02 +0100

    Add MusicCoverView library dependency

--- app/src/main/java/com/sample/andremion/musicplayer/activities/DetailActivity.java
+++ app/src/main/java/com/sample/andremion/musicplayer/activities/DetailActivity.java
@@ -20,23 +20,28 @@ import android.os.Bundle;
 import android.transition.Transition;
 import android.view.View;
 
+import com.andremion.music.MusicCoverView;
 import com.sample.andremion.musicplayer.R;
-import com.sample.andremion.musicplayer.view.CoverView;
 import com.sample.andremion.musicplayer.view.TransitionAdapter;
 
 public class DetailActivity extends PlayerActivity {
 
-    private CoverView mCoverView;
+    private MusicCoverView mCoverView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_detail);
 
-        mCoverView = (CoverView) findViewById(R.id.cover);
-        mCoverView.setCallbacks(new CoverView.Callbacks() {
+        mCoverView = (MusicCoverView) findViewById(R.id.cover);
+        mCoverView.setCallbacks(new MusicCoverView.Callbacks() {
             @Override
-            public void onStopAnimation() {
+            public void onMorphEnd(MusicCoverView coverView) {
+                // Nothing to do
+            }
+
+            @Override
+            public void onRotateEnd(MusicCoverView coverView) {
                 supportFinishAfterTransition();
             }
         });
